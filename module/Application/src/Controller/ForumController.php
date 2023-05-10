79692c3ed59f610bfadbd455ab899eedffbac77f
Jimmy Sole <jimmysole@gmail.com>
2023-05-10 17:11:43 -0400

    controller update

--- module/Application/src/Controller/ForumController.php
+++ module/Application/src/Controller/ForumController.php
@@ -4,18 +4,26 @@
 namespace Application\Controller;
 
 
+use Application\Model\ForumModel;
 use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
 
 class ForumController extends AbstractActionController
 {
-    public function __construct()
-    {
+    public ForumModel $model;
 
+    public ViewModel $viewModel;
+
+
+    public function __construct(ForumModel $model)
+    {
+        $this->model = $model;
+        $this->viewModel = new ViewModel();
     }
 
 
     public function indexAction()
     {
-        
+
     }
 }
